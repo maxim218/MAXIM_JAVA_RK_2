c0dbfe9ed99c682a29c4ba6fd11777f05ab76093
maxim <maxim218maxim218@yandex.ru>
2018-04-05 01:06:15 +0300

    Add adding to database

--- src/main/java/application/controller/Summa.java
+++ src/main/java/application/controller/Summa.java
@@ -1,6 +1,9 @@
 package application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 ///////////////////////////////////////////////////////////
@@ -23,7 +26,7 @@ class TwoNumbers {
 @RequestMapping(path="/summa")
 public class Summa {
     /**
-     * Метод: Post
+     * Метод: POST
      * Url запроса: http://localhost:5000/summa/find_summa?rnd=xxxyyyzzz
      * Тело запроса: {"a":550,"b":125}
      */
@@ -32,6 +35,8 @@ public class Summa {
         int a = body.a;
         int b = body.b;
         int c = a + b;
+        // add to database
+        addRecord(a,b,c);
         return ResponseEntity.ok(c);
     }
 
@@ -42,6 +47,26 @@ public class Summa {
     @RequestMapping(value = "/find_summa", method = RequestMethod.GET)
     public ResponseEntity fff_2(@RequestParam(value="a") int a, @RequestParam(value="b") int b) {
         int c = a + b;
+        // add to database
+        addRecord(a,b,c);
         return ResponseEntity.ok(c);
+    }
+
+
+    //////////////////////////////////////////////////////////
+
+    private NamedParameterJdbcTemplate template = null;
+
+    @Autowired
+    public Summa(NamedParameterJdbcTemplate template) {
+        this.template = template;
+    }
+
+    public void addRecord(int a, int b, int s) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("a", a);
+        params.addValue("b", b);
+        params.addValue("s", s);
+        template.update("INSERT INTO sum_results (a, b, s) VALUES (:a,:b,:s);", params);
     }
 }
---
+++ target/classes/db/migration/V1_1__Description.sql
@@ -0,0 +1,7 @@
+CREATE TABLE sum_results
+(
+  record_id SERIAL PRIMARY KEY,
+  a INTEGER,
+  b INTEGER,
+  s INTEGER
+);
